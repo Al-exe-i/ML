87d5fdf999c5dbd714df49a3447a8336557784cc
Al-exe-i <ax.mishin@yandex.ru>
2024-05-14 22:20:10 +0300

    Добавил линейную регрессию. Остается только гитхаб и api

--- flsite.py
+++ flsite.py
@@ -2,12 +2,13 @@ import pickle
 import numpy as np
 from flask import Flask, render_template, url_for, request
 import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_squared_error, mean_absolute_error, r2_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
@@ -34,7 +35,7 @@ def classification_model_metrics(model: str) -> dict:
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
-    return {"accuracy": accuracy, "precision": precision, "recall": recall}
+    return {"accuracy": round(accuracy, 3), "precision": round(precision, 3), "recall": round(recall, 3)}
 
 
 def make_df(elem1, elem2, elem3) -> pd.DataFrame:
@@ -49,7 +50,7 @@ def index():
 
 
 @app.route("/p_knn", methods=['POST', 'GET'])
-def f_lab1():
+def f_knn():
     if request.method == 'GET':
         return render_template('lab1.html', title="Метод k-ближайших соседей (KNN)", menu=menu, class_model='')
     if request.method == 'POST':
@@ -64,7 +65,7 @@ def f_lab1():
 
 
 @app.route("/p_logistic_regression", methods=['POST', 'GET'])
-def f_lab2():
+def f_log_regression():
     if request.method == 'GET':
         return render_template('lab2.html', title="Логистическая регрессия", menu=menu, class_model='')
     if request.method == 'POST':
@@ -79,7 +80,7 @@ def f_lab2():
 
 
 @app.route("/p_decision_tree", methods=['POST', 'GET'])
-def f_lab3():
+def f_decision_tree():
     if request.method == 'GET':
         return render_template('lab3.html', title="Дерево решений", menu=menu, class_model='')
     if request.method == 'POST':
@@ -93,9 +94,31 @@ def f_lab3():
                                precision=metrics['precision'], recall=metrics['recall'])
 
 
-@app.route("/p_linear_regression")
+@app.route("/p_linear_regression", methods=['POST', 'GET'])
 def f_lab4():
-    return render_template('lab3.html', title="Линейная регрессия", menu=menu)
+    if request.method == 'GET':
+        return render_template('lab4.html', title="Линейная регрессия", menu=menu, class_model='')
+    if request.method == 'POST':
+        ###
+        model = LinearRegression()
+        dataset = pd.read_excel('model/Dataset4.xlsx')
+        dataset['Пол'] = LabelEncoder().fit_transform(dataset['Пол'])
+        x = dataset[['Рост', 'Вес', 'Пол']]
+        y = dataset['Размер обуви']
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        mse = mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        r_2 = r2_score(y_test, y_pred)
+        ###
+        linear_regression = pickle.load(open('model/linear.bin', 'rb'))
+        pred = linear_regression.predict([[float(request.form['list1']), float(request.form['list2']),
+                                           float(request.form['list3'])]])
+        return render_template('lab4.html', title="Линейная регрессия", menu=menu,
+                               class_model=round(pred[0]),
+                               mae=mae,
+                               mse=mse, r_2=r_2)
 
 
 if __name__ == "__main__":
